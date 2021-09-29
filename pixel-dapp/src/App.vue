74e464af03650d871c6c854c067f032fe713a87c
Isaac Murtagh <isaac.murtagh@centrapay.com>
2021-09-30 08:56:10 +1300

    Change color picker

--- pixel-dapp/src/App.vue
+++ pixel-dapp/src/App.vue
@@ -5,11 +5,8 @@
     </div>
     <h2>Pixel Dapp</h2>
     <div class="flex">
-      <ColorPicker
-        theme="light"
-        :color="color"
-        @changeColor="changeColor"
-      />
+
+      <chrome-picker v-model="color" />
 
       <div style="margin-left: 2rem;">
         <p>The smart contract this relies on is only deployed to the <b>Ropsten Test Network</b></p>
@@ -34,15 +31,16 @@
       v-if="canvasLoaded"
       :canvas-matrix="canvasMatrix"
       :canvas-client="canvasClient"
-      :selected-color="color"
+      :selected-color="hexColor"
     />
   </div>
 </template>
 
 <script>
 
-import { ColorPicker } from 'vue-color-kit'
-import 'vue-color-kit/dist/vue-color-kit.css'
+// import { ColorPicker } from 'vue-color-kit'
+// import 'vue-color-kit/dist/vue-color-kit.css'
+import { Chrome } from '@ckpack/vue-color'
 import Canvas from './contracts/Canvas.js';
 import CanvasComponent from './components/Canvas.vue';
 import getWeb3 from './web3Client.js';
@@ -51,7 +49,7 @@ export default {
 
   components: {
     CanvasComponent,
-    ColorPicker
+    'chrome-picker': Chrome
   },
 
   data() {
@@ -60,7 +58,7 @@ export default {
       clientError: '',
       canvasLoaded: false,
       canvasMatrix: [],
-      color: '#59c7f9'
+      color: { hex: '#59c7f9' }
     }
   },
 
@@ -71,6 +69,12 @@ export default {
     const web3Client = await getWeb3();
     this.canvasClient = await Canvas.getClient(web3Client);
     await this.getCanvasPixels();
+  },
+
+  computed: {
+    hexColor() {
+      return this.color.hex;
+    }
   },
 
   methods: {
@@ -92,10 +96,6 @@ export default {
         return false;
       }
       return true;
-    },
-
-    changeColor(color) {
-      this.color = color.hex
     },
 
     
